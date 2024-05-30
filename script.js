4efdad857610c7c8357c1fe605060a1e30f28377
Shyam20 <shyampadiyar38@gmail.com>
2024-05-30 20:16:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var typed = new typed(".text",{
+//     Strings:["Frontend Developer" , "Coder" , "Web Developer"] ,
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop: true
+// });
+
+function scl(){
+    a = document.getElementById("home").style.background-image;
+    a = "none";
+}
+
